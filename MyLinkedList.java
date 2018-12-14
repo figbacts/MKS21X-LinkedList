1ed7dc8ce1d8657ff5ec987317945694249dc618
Jack Lam <jlam00@stuy.edu>
2018-12-14 10:58:55 -0500

    added add size and tostring

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -33,9 +33,46 @@ class MyLinkedList{
  private int size;
  private Node start,end;
  public MyLinkedList(){
-
+   start = new Node(null, null, null);
+   end = new Node (null,null,null)
  }
- public int size();
- public boolean add(int value);
- public String toString();
+
+
+ public int size(){
+   return size;
+ }
+
+
+ public boolean add(int value){
+   if(size = 0){
+     Node add = new Node(null,value,null);
+     start.setNext(add);
+     end.setPrev(add);
+     size += 1;
+    return true;
+   }
+   else{
+   Node add = new Node(end.prev(), value, null);
+   end.setPrev(add)
+   size += 1;
+   return true;
+ }
+ return false;
+ }
+
+ public String toString(){
+   if (size = 0){
+     return "[]";
+   }
+   String ans = "["
+   Node current = start.next();
+   while (current.getData() != null){
+   ans = ans + current.getData(); + ", "
+   current = current.next();
+    }
+    ans = ans + "]"
+    return ans;
+ }
+
+ 
 }
